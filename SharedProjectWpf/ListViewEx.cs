671ea363990a7c3933988e2658ce5b43a25f533f
Sean Griffin <seangriffin@internode.on.net>
2022-09-03 00:01:06 +1000

    Add files via upload

---
+++ SharedProjectWpf/ListViewEx.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace SharedProject
+{
+    public static class ListViewEx
+    {
+
+
+
+        public static void FromQuery<T>(this ListView lv, string mysql_query, string db) where T : new()
+        {
+            var dbConnect = new DBConnect(db);
+            var result = dbConnect.Select<T>(mysql_query, false);
+            lv.ItemsSource = null;
+            lv.ItemsSource = result;
+        }
+
+
+    }
+}
